544daf97b92c41540cd388cb1f50cd9d43a44e52
nikeshbhagalia <14082394+nikeshbhagalia@users.noreply.github.com>
2020-05-04 21:19:31 +1200

    Sorted Using

--- cricketapi/Models/PlayerImageItem.cs
+++ cricketapi/Models/PlayerImageItem.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 
 namespace cricketapi.Models
 {
