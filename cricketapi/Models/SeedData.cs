666fd0bfdadc6dac45967811fcbe26aac9247985
nikeshbhagalia <14082394+nikeshbhagalia@users.noreply.github.com>
2020-06-24 19:41:35 +1200

    Sorted using

--- cricketapi/Models/SeedData.cs
+++ cricketapi/Models/SeedData.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace cricketapi.Models
 {
