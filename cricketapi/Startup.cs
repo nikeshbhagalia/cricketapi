f4dd9361e9ea31859c7d0eecaf5311c8474e57e1
nikeshbhagalia <14082394+nikeshbhagalia@users.noreply.github.com>
2020-09-06 18:03:03 +1200

    Remove comment

--- cricketapi/Startup.cs
+++ cricketapi/Startup.cs
@@ -38,7 +38,6 @@ namespace cricketapi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseHsts();
