ac96ccae19b61b38ec0ff0d1deece31e9555d7e4
nikeshbhagalia <14082394+nikeshbhagalia@users.noreply.github.com>
2020-10-15 14:38:17 +1300

    Sorted using

--- cricketapi/Program.cs
+++ cricketapi/Program.cs
@@ -1,11 +1,11 @@
 ﻿using cricketapi.Models;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
