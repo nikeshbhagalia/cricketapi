7cf4224f42557888af2d39c9aeab1ef9ac20ef46
nikeshbhagalia <14082394+nikeshbhagalia@users.noreply.github.com>
2020-10-19 18:57:24 +1300

    Sort using

--- cricketapi/Program.cs
+++ cricketapi/Program.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 
 namespace cricketapi
 {
